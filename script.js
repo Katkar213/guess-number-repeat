4171dd65e288d9ef85b7fadb7660698f42492374
katkar213 <blackship213@gmail.com>
2023-07-14 21:21:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let body=document.querySelector("body");
+let randomno=random(100);
+let inputValue=document.getElementById("input");
+let chance=document.getElementsByClassName("chance1");
+let highscore=document.getElementsByClassName("highscore1")
+let Massage=document.getElementsByClassName("changetext")
+let Qst=document.getElementsByClassName("Qstmark")
+
+function random(n){
+return Math.floor((Math.random()*n))+1;
+}
+console.log(randomno);
+count=100;
+function check(){
+    let ans=parseInt(inputValue.value);
+    console.log(parseInt(inputValue.value));
+    
+   
+ if(ans>randomno){
+   Massage[0].innerText="Your Guess is High.."
+   count--;
+   chance[0].innerText=`${count}`;
+ }
+
+ else if(ans<randomno){
+    Massage[0].innerText="Your Guess is Low.."
+    count--;
+    console.log(count);
+    chance[0].innerText=`${count}`;
+  }
+
+else{
+    Massage[0].innerText="Hurray You Won!!.."
+    count--;
+   chance[0].textContent=`${count}`;
+   highscore[0].innerText=count;
+   body.style.backgroundColor="green";
+   
+}
+if(count===0){
+  Qst[0].innerText=randomno;
+}
+}
+
+
+function reset(){
+    count=100;
+    Massage[0].innerText="Start guessing..."
+    chance[0].innerText=`${count}`;
+    inputValue.value=null;
+    body.style.backgroundColor="lightgrey"
+}
